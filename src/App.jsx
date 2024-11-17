c770b4d3235bb43f107310f1a1e1d1dfa437d1af
cendyz <arek57823@gmail.com>
2024-11-17 09:21:07 +0100

    mobile design

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+const App = () => {
+	return (
+		<main className='main'>
+			<div className='main-box'>
+				<p className='main-advice-number'>Advice #333</p>
+				<p className='main-advice-text'>
+					"It is easy to sit up and take notice, what's difficult is getting up and taking action."
+				</p>
+				<img src='src/images/pattern-divider-mobile.svg' alt='Divider icon' className='main-line-img' />
+			</div>
+
+			<div className='dice-box'>
+				<img src='src/images/icon-dice.svg' alt='Dice icon' className='dice-icon' />
+			</div>
+		</main>
+	);
+};
+export default App;
---
+++ src/data.js
@@ -0,0 +1,46 @@
+import toDoListIcon from "./images/icon-todo.svg";
+import calendarIcon from "./images/icon-calendar.svg";
+import remindersIcon from "./images/icon-reminders.svg";
+import planningIcon from "./images/icon-planning.svg";
+
+export const firstListItems = [
+	{
+		id: 1,
+		img: toDoListIcon,
+		alt: "Note icon",
+		name: "Todo List",
+	},
+	{
+		id: 2,
+		img: calendarIcon,
+		alt: "Calendar icon",
+		name: "Calendar",
+	},
+	{
+		id: 3,
+		img: remindersIcon,
+		alt: "Bulb icon",
+		name: "Reminders",
+	},
+	{
+		id: 4,
+		img: planningIcon,
+		alt: "Planner icon",
+		name: "Planning",
+	},
+];
+
+export const secondListItems = [
+	{
+		id: 1,
+		name: "History",
+	},
+	{
+		id: 2,
+		name: "Our Team",
+	},
+	{
+		id: 3,
+		name: "Blog",
+	},
+];
